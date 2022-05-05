84937fdf2e1eafdaccb1047041c358ae5e26bd9c
ferh0162 <ferh0162@stud.kea.dk>
2022-05-05 10:53:22 +0200

    orderDiscount metode oprettet.

--- src/com/company/Controller.java
+++ src/com/company/Controller.java
@@ -1,12 +1,28 @@
 package com.company;
 
+import java.util.Locale;
+
 public class Controller {
 
   public void run(){
     boolean running = true;
-
     while (running){
 
     }
+  }
+  public double orderDicount(String state){
+    state = state.toUpperCase(Locale.ROOT);
+    double discount = 0;
+
+    switch (state){
+      case "UT" ->	discount = 6.85;
+      case "NV" ->  discount = 8.00;
+      case "TX" ->  discount = 6.25;
+      case "AL" ->  discount = 4.00;
+      case "CA"	->  discount = 8.25;
+    }
+    System.out.println(discount);
+    return discount;
+
   }
 }
