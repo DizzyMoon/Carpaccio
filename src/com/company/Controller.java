0b2f2f76af470c78cac452c3a48b3a38530f8410
DizzyMoon <mikkel.simons1@gmail.com>
2022-05-05 10:43:13 +0200

    Added Order Class

--- src/com/company/Controller.java
+++ src/com/company/Controller.java
@@ -1,4 +1,12 @@
 package com.company;
 
 public class Controller {
+
+  public void run(){
+    boolean running = true;
+
+    while (running){
+
+    }
+  }
 }
---
+++ src/com/company/Order.java
@@ -0,0 +1,25 @@
+package com.company;
+
+public class Order {
+  private int amount;
+  private double price;
+  private String state;
+
+  public Order(int amount, double price, String state){
+    this.amount = amount;
+    this.price = price;
+    this.state = state;
+  }
+
+  public int getAmount(){
+    return amount;
+  }
+
+  public double getPrice(){
+    return price;
+  }
+
+  public String getState(){
+    return state;
+  }
+}
